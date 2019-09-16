08f8d118811fd5007833b6a2b7d1dbfae4d6020e
ArletaStekla <36191647+ArletaStekla@users.noreply.github.com>
2019-09-16 19:58:11 +0200

    Upload script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿function createPagination() {
+    var element = document.getElementsByTagName("li"),
+        index;
+
+    for (index = element.length - 1; index >= 0; index--) {
+        element[index].parentNode.removeChild(element[index]);
+    }
+
+    let pageAmount = parseInt(document.getElementById("pageAmount").value);
+    let offset = parseInt(document.getElementById("offset").value);
+
+    for (let i = 0; i < pageAmount; i++) {
+        var page = i + 1;
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode("Page " + page);
+        node.appendChild(textnode);
+        document.getElementById("myList").appendChild(node);
+    }
+    var listObj = new List('test-list', {
+        page: 1,
+        pagination: {
+            innerWindow: offset
+        }
+    });
+}
